78fc139f64b027fd1c1d1110189e957cb6f7cfa5
ja-odur <odurjoseph8@gmail.com>
2025-07-04 01:02:05 +0300

    Add navigation between raflley links and pages

--- lib/raffley_web/live/raffle_live/show.ex
+++ lib/raffley_web/live/raffle_live/show.ex
@@ -54,7 +54,9 @@ defmodule RaffleyWeb.RaffleLive.Show do
       <h4>Featured Raffles</h4>
       <ul class="raffles">
         <li :for={raffle <- @raffles}>
-          <img src={raffle.image_path} />
+          <.link navigate={~p"/raffles/#{raffle}"}>
+            <img src={raffle.image_path} />
+          </.link>
         </li>
       </ul>
     </section>
