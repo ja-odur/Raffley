e5d05cc994ad713221c1ede8f8640a141251d8d8
ja-odur <odurjoseph8@gmail.com>
2025-07-08 00:10:02 +0300

    Handle form errors

--- lib/raffley_web/live/raffle_live/admin_raffle_live/form.ex
+++ lib/raffley_web/live/raffle_live/admin_raffle_live/form.ex
@@ -1,11 +1,16 @@
 defmodule RaffleyWeb.AdminRaffleLive.Form do
   use RaffleyWeb, :live_view
 
+  alias Raffley.Admin
+  alias Raffley.Raffles.Raffle
+
   def mount(_params, _session, socket) do
+    changeset = Raffle.changeset(%Raffle{}, %{})
+
     socket =
       socket
       |> assign(:page_title, "New Raffle")
-      |> assign(:form, to_form(%{}, as: "raffle"))
+      |> assign(:form, to_form(changeset))
 
     {:ok, socket}
   end
@@ -15,7 +20,7 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
     <.header>
       {@page_title}
     </.header>
-    <.simple_form for={@form} id="raffle-form">
+    <.simple_form for={@form} id="raffle-form" phx-submit="save">
       <.input field={@form[:prize]} label="Prize" />
       <.input field={@form[:description]} type="textarea" label="Description" />
       <.input field={@form[:ticket_price]} type="number" label="Ticket Price" />
@@ -29,7 +34,7 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
       <.input field={@form[:image_path]} label="Image Path" />
 
       <:actions>
-        <.button>Save Raffle</.button>
+        <.button phx-disable-with="Saving...">Save Raffle</.button>
       </:actions>
     </.simple_form>
 
